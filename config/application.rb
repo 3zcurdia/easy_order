bb304825fc9009abd252fc12e3275f1fe12dd6c5
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-07-14 16:22:09 -0500

    Setup spanish as default locale and include localizations

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module EasyOrder
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.i18n.default_locale = :es
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
