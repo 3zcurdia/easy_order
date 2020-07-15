10cab768f97bae94aeefae39566e25676b9138df
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-07-14 23:35:35 -0500

    Add build wizard mvp steps

--- app/controllers/build/menus_controller.rb
+++ app/controllers/build/menus_controller.rb
@@ -1,3 +1,41 @@
 class Build::MenusController < ApplicationController
-  def show; end
+  before_action :validate_step
+
+  def new
+    @menu = merchant.menu
+    if @menu.items.count.zero?
+      4.times do
+        @menu.items.build
+      end
+    end
+  end
+
+  def show
+    @menu = merchant.menu
+  end
+
+  def update
+    @menu = merchant.menu
+    if @menu.update(menu_attributes)
+      redirect_to build_preview_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def menu_attributes
+    params.require(:menu).permit(items_attributes: %i[id name price])
+  end
+
+  def validate_step
+    return if session[:guest_merchant_id].present?
+
+    redirect_to new_build_merchant_path
+  end
+
+  def merchant
+    @merchant ||= Merchant.find(session[:guest_merchant_id])
+  end
 end
--- app/controllers/build/previews_controller.rb
+++ app/controllers/build/previews_controller.rb
@@ -1,3 +1,15 @@
 class Build::PreviewsController < ApplicationController
-  def show; end
+  before_action :validate_step
+
+  def show
+    @merchant = Merchant.includes(menu: :items).find(session[:guest_merchant_id])
+  end
+
+  private
+
+  def validate_step
+    return if session[:guest_merchant_id].present?
+
+    redirect_to new_build_merchant_path
+  end
 end
