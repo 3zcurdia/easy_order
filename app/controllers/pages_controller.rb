38e7820b935d3f258c8ad057f08280949497ddc2
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-07-16 08:48:00 -0500

    Respond QR images in page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,33 @@
 class PagesController < ApplicationController
   layout 'landing_page'
+  before_action :set_merchant
 
   def show
+    respond_to do |format|
+      format.html { render :show }
+      format.svg  { render inline: svg_file }
+      format.png  { render file: png_file, layout: false }
+    end
+  end
+
+  private
+
+  def set_merchant
     @merchant = Merchant.friendly.find(params[:id])
+  end
+
+  def code
+    @code ||= RQRCode::QRCode.new(page_url(@merchant))
+  end
+
+  def svg_file
+    code.as_svg
+  end
+
+  def png_file
+    filepath = "tmp/#{@merchant.slug}.png"
+    return filepath if File.exist?(filepath)
+    code.as_png(size: 512).save(filepath)
+    filepath
   end
 end
