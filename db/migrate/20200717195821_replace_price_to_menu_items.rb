b8334d86db50ef46ca1b660e64ad4191a534b57a
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-07-17 15:28:32 -0500

    Fix migration

--- db/migrate/20200717195821_replace_price_to_menu_items.rb
+++ db/migrate/20200717195821_replace_price_to_menu_items.rb
@@ -1,6 +1,7 @@
 class ReplacePriceToMenuItems < ActiveRecord::Migration[6.0]
   def change
-    change_column :menu_items, :currency, :price_currency, :string, default: 'MXN'
+    rename_column :menu_items, :currency, :price_currency
+    change_column_default :menu_items, :price_currency, 'MXN'
     change_column :menu_items, :price_cents, :integer, null: false, default: 0
   end
 end
