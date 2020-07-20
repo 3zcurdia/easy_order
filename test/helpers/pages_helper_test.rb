4d85609ac13f74135cd25fd2ca0faba4072e53d1
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-07-20 16:50:52 -0500

    Fix broken tests

--- test/helpers/pages_helper_test.rb
+++ test/helpers/pages_helper_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class PagesHelperTest < ActionView::TestCase
   test 'remove spaces in text' do
     result = whatsapp_url('5555555555', 'phrase with spaces')
-    assert_equal 'https://wa.me/525555555555?text=phrase+with+spaces', result
+    assert_equal 'https://wa.me/5555555555?text=phrase+with+spaces', result
   end
 end
