b4f0b98940613afa2424a9b46fe16a87c0f7055f
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-07-21 16:43:01 -0500

    Implement merchant logo in show views

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -56,6 +56,6 @@ class MerchantsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def merchant_params
-    params.require(:merchant).permit(:name, :phone, :delivery, :keyword_list)
+    params.require(:merchant).permit(:name, :phone, :delivery, :keyword_list, :logo)
   end
 end
