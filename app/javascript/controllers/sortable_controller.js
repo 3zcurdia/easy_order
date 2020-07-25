fd369b4dfaf0211142a3125b9cfdb1e441ab7c73
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-07-25 00:10:06 -0500

    Fix merchant dashboard layout

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -9,7 +9,6 @@ export default class extends Controller {
     var sortable = Sortable.create(
       this.element,
       {
-        handle: '.handle',
         onEnd: function (event) { that.update(event); },
       }
     )
