b8d1ed993c0e7f8b8806990d62e132026c001775
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-07-28 11:29:01 -0500

    Lint db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,114 +11,113 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_07_28_162415) do
-
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.bigint 'byte_size', null: false
+    t.string 'checksum', null: false
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  create_table 'friendly_id_slugs', force: :cascade do |t|
+    t.string 'slug', null: false
+    t.integer 'sluggable_id', null: false
+    t.string 'sluggable_type', limit: 50
+    t.string 'scope'
+    t.datetime 'created_at'
+    t.index %w[slug sluggable_type scope], name: 'index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope', unique: true
+    t.index %w[slug sluggable_type], name: 'index_friendly_id_slugs_on_slug_and_sluggable_type'
+    t.index %w[sluggable_type sluggable_id], name: 'index_friendly_id_slugs_on_sluggable_type_and_sluggable_id'
   end
 
-  create_table "menu_items", force: :cascade do |t|
-    t.bigint "menu_id", null: false
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.jsonb "meta", default: {}
-    t.string "price_currency", default: "MXN"
-    t.integer "price_cents", default: 0, null: false
-    t.bigint "section_id"
-    t.integer "position", default: 0
-    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
-    t.index ["section_id"], name: "index_menu_items_on_section_id"
+  create_table 'menu_items', force: :cascade do |t|
+    t.bigint 'menu_id', null: false
+    t.string 'name'
+    t.text 'description'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.jsonb 'meta', default: {}
+    t.string 'price_currency', default: 'MXN'
+    t.integer 'price_cents', default: 0, null: false
+    t.bigint 'section_id'
+    t.integer 'position', default: 0
+    t.index ['menu_id'], name: 'index_menu_items_on_menu_id'
+    t.index ['section_id'], name: 'index_menu_items_on_section_id'
   end
 
-  create_table "menus", force: :cascade do |t|
-    t.bigint "merchant_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["merchant_id"], name: "index_menus_on_merchant_id"
+  create_table 'menus', force: :cascade do |t|
+    t.bigint 'merchant_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['merchant_id'], name: 'index_menus_on_merchant_id'
   end
 
-  create_table "merchants", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "slug"
-    t.bigint "user_id", null: false
-    t.string "phone"
-    t.float "latitude"
-    t.float "longitude"
-    t.jsonb "info", default: {}
-    t.index ["latitude", "longitude"], name: "index_merchants_on_latitude_and_longitude"
-    t.index ["slug"], name: "index_merchants_on_slug", unique: true
-    t.index ["user_id"], name: "index_merchants_on_user_id"
+  create_table 'merchants', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'slug'
+    t.bigint 'user_id', null: false
+    t.string 'phone'
+    t.float 'latitude'
+    t.float 'longitude'
+    t.jsonb 'info', default: {}
+    t.index %w[latitude longitude], name: 'index_merchants_on_latitude_and_longitude'
+    t.index ['slug'], name: 'index_merchants_on_slug', unique: true
+    t.index ['user_id'], name: 'index_merchants_on_user_id'
   end
 
-  create_table "sections", force: :cascade do |t|
-    t.bigint "menu_id", null: false
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "position", default: 0
-    t.integer "menu_items_count", default: 0
-    t.index ["menu_id"], name: "index_sections_on_menu_id"
+  create_table 'sections', force: :cascade do |t|
+    t.bigint 'menu_id', null: false
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'position', default: 0
+    t.integer 'menu_items_count', default: 0
+    t.index ['menu_id'], name: 'index_sections_on_menu_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "role", default: 0
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.string "provider"
-    t.string "uid"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'role', default: 0
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.inet 'current_sign_in_ip'
+    t.inet 'last_sign_in_ip'
+    t.string 'provider'
+    t.string 'uid'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "menu_items", "menus"
-  add_foreign_key "menu_items", "sections"
-  add_foreign_key "menus", "merchants"
-  add_foreign_key "merchants", "users"
-  add_foreign_key "sections", "menus"
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'menu_items', 'menus'
+  add_foreign_key 'menu_items', 'sections'
+  add_foreign_key 'menus', 'merchants'
+  add_foreign_key 'merchants', 'users'
+  add_foreign_key 'sections', 'menus'
 end
