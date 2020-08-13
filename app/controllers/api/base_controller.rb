9e19861edb1085f44a0f6925f062a76474091838
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-08-13 15:01:19 -0500

    Set api with default json format

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,10 @@
 module Api
   class BaseController < ActionController::API
     include Pundit
+    before_action :default_format_json
+
+    def default_format_json
+      request.format = 'json'
+    end
   end
 end
