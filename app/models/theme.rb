8f5c6adea50fe952bac55e38a525028a8064f000
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-08-23 00:37:43 -0500

    Fix error on theme load

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,6 +1,8 @@
 Theme = Struct.new(:header_background, keyword_init: true) do
   extend ActiveModel::Naming
-  def self.parse(str)
-    new(str || {})
+  def self.parse(hash)
+    attributes = Theme.new.to_h.keys
+    hash = hash.select { |k, _v| attributes.include?(k.to_sym) }
+    new(hash || {})
   end
 end
