6ac38e4720dd32e484680a821240a6d1fde255bc
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-08-26 13:23:24 -0500

    Fix nil class error

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,7 +2,7 @@ Theme = Struct.new(:header_background, keyword_init: true) do
   extend ActiveModel::Naming
   def self.parse(hash)
     attributes = Theme.new.to_h.keys
-    hash = hash.select { |k, _v| attributes.include?(k.to_sym) }
+    hash = (hash || {}).select { |k, _v| attributes.include?(k.to_sym) }
     new(hash || {})
   end
 end
