d92c867c7ee94a8ae8e1ce2abff2894a9dd092f1
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-10-24 12:13:45 -0500

    Allow to delete items with orders

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -3,6 +3,7 @@ class MenuItem < ApplicationRecord
   belongs_to :menu, touch: true
   belongs_to :section, counter_cache: true, touch: true
   has_one_attached :photo
+  has_many :order_items, dependent: :destroy
 
   validates :name, presence: true
 
