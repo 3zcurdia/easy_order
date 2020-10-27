88bba8fca8bb86ef632ee154013b4790c9faa1e7
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-10-27 15:14:22 -0600

    Add unauthorized fallback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized
+    flash[:alert] = "No estas autorizado para realizar esta acción."
+    redirect_to(request.referrer || root_path)
+  end
 end
