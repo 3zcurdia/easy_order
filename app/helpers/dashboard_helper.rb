01546b4390ffa24845b401f1a95ba2d9d15674e4
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-10-27 14:41:27 -0600

    Fix QR in cards view

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,5 +1,5 @@
 module DashboardHelper
   def svg_code(merchant, size = 5)
-    raw RQRCode::QRCode.new(merchant_code_url(merchant)).as_svg(module_size: size)
+    raw RQRCode::QRCode.new(page_url(merchant)).as_svg(module_size: size)
   end
 end
