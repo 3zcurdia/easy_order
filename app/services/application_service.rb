4d61ddbeed12da1a6f1d5b6db63c76f7078e753d
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-11-18 23:00:36 -0600

    Fix linter error

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationService
-  def self.call(*args, &block)
-    new(*args, &block).call
+  def self.call(...)
+    new(...).call
   end
 
-  def self.to_proc(*args, &block)
-    proc { |*args| self.new(*args, &block).call }
+  def self.to_proc(*_args, &block)
+    proc { |*args| new(*args, &block).call }
   end
 end
