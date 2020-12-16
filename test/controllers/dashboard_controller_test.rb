2336359b1e5de9b3b42dde928e96aae7c1252df5
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-12-16 11:28:37 -0600

    Add simple sucessfull test and fix rails 6.1 active storage errors  (#112)
    * Add simple sucessful test
    
    * Update rails 6.1 defaults
    
    * Fix linter errors

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -7,10 +7,19 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
     @user ||= create(:user, :merchant)
   end
 
-  test 'should render show' do
-    create(:merchant, user: user)
+  setup do
     sign_in(user)
+  end
+
+  test 'should redirect to show' do
+    create(:merchant, user: user)
     get user_root_url
     assert_response :redirect
+  end
+
+  test 'should render a succesfull response' do
+    merchant = create(:merchant, user: user)
+    get merchant_path(merchant)
+    assert_response :success
   end
 end
