86d43370c38e619317d9d7be77787f163d714995
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2021-02-16 09:19:26 -0600

    Fix Style/StringLiterals in time zone config

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module EasyOrder
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Central Time (US & Canada)'
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
